ae0788f37d7a663fa253d79fe03bf103a23b36e9
RMH <riki@craftsmoothie.co.nz>
2018-12-22 22:47:42 +1300

    create varibles for each button

--- pseudocode.js
+++ pseudocode.js
@@ -1,74 +1,119 @@
-// create a varible = to an array for the users inputs
-// create a varible = 0
-// create a varible to temporarily que numbers and symbols
-// create varible for value of input
-var entries = [];
-var total = 0;
 
-var temp = '';
-$("button").on('click', function() {
- 	var val = $(this).text();
+var oneBtn = document.getElementById('one');
+var twoBtn = document.getElementById('two');
+var threeBtn = document.getElementById('three');
+var fourBtn = document.getElementById('four');
+var fiveBtn = document.getElementById('five');
+var sixBtn = document.getElementById('six');
+var sevenBtn = document.getElementById('seven');
+var eightBtn = document.getElementById('eight');
+var nineBtn = document.getElementById('nine');
+var zeroBtn = document.getElementById('zero');
 
-  // Got a number, add to temp
-  if (!isNaN(val) || val === '.') {
-    temp += val;
-    $("#answer").val(temp.substring(0,10));
+var decimalBtn = document.getElementById('decimal');
+var clearBtn = document.getElementById('AC');
+var backspaceBtn = document.getElementById('CE');
+var displayValElement = document.getElementById('display');
+
+var displayVal = '0';     // number or symbol displayed
+var pendingVal;           // values pending
+var evalStringArray = []; // ques previous typed numbers and symbols
+
+var numberBtns = document.getElementsByClassName('btn number');
+var symbolBtns = document.getElementsByClassName('btn symbol');
+
+var updateDisplayVal = (clickObj) => {
+  var btnText = clickObj.target.innerText;
+
+  if (displayVal === '0')
+    displayVal = '';
+
+    displayVal += btnText;
+    displayValElement.innerText = displayVal;
+}
+
+for (let i = 0; i < numberBtns.length; i++) {
+  numberBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+
+// for (let i = 0; i < symbolBtns.length; i++) {
+//   symbolBtns[i].addEventListener('click', performOperation, false);
+// }
+
+
+
+
+// // create a varible = to an array for the users inputs
+// // create a varible = 0
+// // create a varible to temporarily que numbers and symbols
+// // create varible for value of input
+// var entries = [];
+// var total = 0;
+
+// var temp = '';
+// $("button").on('click', function() {
+//  	var val = $(this).text();
+
+//   // Got a number, add to temp
+//   if (!isNaN(val) || val === '.') {
+//     temp += val;
+//     $("#answer").val(temp.substring(0,10));
     
-  // Got some symbol other than equals, add temp to our entries
-  // then add our current symbol and clear temp
-  } else if (val === 'AC') {
-    entries = [];
-    temp = '';
-    total = 0;
-    $("#answer").val('')
+//   // Got some symbol other than equals, add temp to our entries
+//   // then add our current symbol and clear temp
+//   } else if (val === 'AC') {
+//     entries = [];
+//     temp = '';
+//     total = 0;
+//     $("#answer").val('')
 
-  // Clear last entry
-  } else if (val === 'CE') {
-    temp = '';
-    $("#answer").val('')
+//   // Clear last entry
+//   } else if (val === 'CE') {
+//     temp = '';
+//     $("#answer").val('')
     
-  // Change multiply symbol to work with eval
-  } else if (val === 'x') {
-    entries.push(temp);
-    entries.push('*');
-    temp = '';
+//   // Change multiply symbol to work with eval
+//   } else if (val === 'x') {
+//     entries.push(temp);
+//     entries.push('*');
+//     temp = '';
     
-  // Change divide symbol to work with eval
-  } else if (val === '÷') {
-    entries.push(temp);
-    entries.push('/');
-    temp = '';
+//   // Change divide symbol to work with eval
+//   } else if (val === '÷') {
+//     entries.push(temp);
+//     entries.push('/');
+//     temp = '';
 
-  // Got the equals sign, perform calculation
-  } else if (val === '=') {
-  	entries.push(temp);
+//   // Got the equals sign, perform calculation
+//   } else if (val === '=') {
+//   	entries.push(temp);
 
-    var nt = Number(entries[0]);
-    for (var i = 1; i < entries.length; i++) {
-      var nextNum = Number(entries[i+1])
-      var symbol = entries[i];
+//     var nt = Number(entries[0]);
+//     for (var i = 1; i < entries.length; i++) {
+//       var nextNum = Number(entries[i+1])
+//       var symbol = entries[i];
       
-      if (symbol === '+') { nt += nextNum; } 
-      else if (symbol === '-') { nt -= nextNum; } 
-      else if (symbol === '*') { nt *= nextNum; } 
-      else if (symbol === '/') { nt /= nextNum; }
+//       if (symbol === '+') { nt += nextNum; } 
+//       else if (symbol === '-') { nt -= nextNum; } 
+//       else if (symbol === '*') { nt *= nextNum; } 
+//       else if (symbol === '/') { nt /= nextNum; }
       
-      i++;
-    }
+//       i++;
+//     }
     
-    // Swap the '-' symbol so text input handles it correctly
-    if (nt < 0) {
-      nt = Math.abs(nt) + '-';
-    }
+//     // Swap the '-' symbol so text input handles it correctly
+//     if (nt < 0) {
+//       nt = Math.abs(nt) + '-';
+//     }
     
-    $("#answer").val(nt);
-		entries = [];
-    temp = '';
+//     $("#answer").val(nt);
+// 		entries = [];
+//     temp = '';
     
-  // Push number
-  } else {
-    entries.push(temp);
-    entries.push(val);
-    temp = '';
-  }
-});
+//   // Push number
+//   } else {
+//     entries.push(temp);
+//     entries.push(val);
+//     temp = '';
+//   }
+// });
